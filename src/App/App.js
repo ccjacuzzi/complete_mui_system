b510be153742dee19c6d6eda9ee870fcbd465159
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-12-17 07:57:29 -0600

    Added ThemeProvider as suggested in "migrating to v5: getting started".

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import { Box, CssBaseline, createTheme } from '@mui/material';
 import PageHeader from '../Components/PageHeader';
 import { PeopleOutlineTwoTone } from '@mui/icons-material';
 import Employees from '../Employees/Employees';
+import { ThemeProvider } from '@mui/styles'; //in MUI v5, ThemeProvider is required to use makeStyles()
 
 
 
@@ -20,7 +21,7 @@ function App() {
 
   
   return (
-    <>    
+    <ThemeProvider>    
       <SideBar />
       <Box sx={styles.appMain}>
         <Header />
@@ -28,7 +29,7 @@ function App() {
         <Employees />
       </Box>
       <CssBaseline /> {/* CssBaseLine applies common css rules that are pretty much always used */}
-    </>
+    </ThemeProvider>
 
   );
 }
